267c5c2fa3e26a7cb8c81646a4702390dbde0be0
Dhananjay Singh <dhananjaysingh3010@gmail.com>
2020-09-04 20:33:43 +0530

    template ref and also viewchil use

--- component-practice/src/app/cockpit/cockpit.component.ts
+++ component-practice/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 // import { EventEmitter } from 'protractor';
 
 @Component({
@@ -9,20 +9,36 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
 
   // serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
 
   /*Same event name as in appcomp.html */
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+      /* We will access server name  just via Template Ref Variable and server content via view child so that we understand both the ways. 
+    We can eith use only template ref Or view child for both aslo */
+
+    @ViewChild('newServerContentInput') newServerContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServerHandler() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // onAddServerHandler() {
+  //   this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    // this.serverElements.push({
+    //   type: 'Server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  // }
+
+  onAddServerHandler(newServerNameInput: HTMLInputElement) {
+    /* We will access server name  just via Template Ref Variable and server content via view child so that we understand both the ways. 
+    We can eith use only template ref Or view child for both aslo */
+    this.serverCreated.emit({serverName: newServerNameInput.value, serverContent: this.newServerContentInput.nativeElement.value});
     // this.serverElements.push({
     //   type: 'Server',
     //   name: this.newServerName,
@@ -30,12 +46,22 @@ export class CockpitComponent implements OnInit {
     // });
   }
 
-  onAddBlueprintHandler() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // onAddBlueprintHandler() {
+  //   this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    // this.serverElements.push({
+    //   type: 'Blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  // }
+
+  onAddBlueprintHandler(newServerNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: newServerNameInput.value, serverContent: this.newServerContentInput.nativeElement.value});
     // this.serverElements.push({
     //   type: 'Blueprint',
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
   }
+
 }
